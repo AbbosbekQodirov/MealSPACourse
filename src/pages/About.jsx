f83ad0c1cc6cd6f16aa095446a98e64653fc7457
“Abbosbek” <abbosbekqodirov2501@gmail.com>
2023-08-16 12:13:51 +0500

    about, contact added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div>
+      <h2 className='about'>
+        Hello, my name is Abbasbek. I am a Frontend (React.js) developer.
+        Currently, I am a teacher of Frontend programming at Codial IT Academy.🔥🔥💻.
+      </h2>
+    </div>
+  );
 }
 
 export default About
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import {BsTelephoneOutbound} from "react-icons/bs"
+import { GiEarthAmerica } from "react-icons/gi";
+import { LiaTelegram } from "react-icons/lia";
+import { FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="contact">
+      <a href="tel:+998911612501">
+        <h4>
+          <BsTelephoneOutbound /> +998 91 161 25 01
+        </h4>
+      </a>
+      <a href="https://t.me/abbosbek_qodirovv">
+        <h4>
+          <LiaTelegram /> abbosbek_qodirovv
+        </h4>
+      </a>
+      <a href="https://www.instagram.com/abbosbek_qodirovv/">
+        <h4>
+          <FaInstagram /> abbosbek_qodirovv
+        </h4>
+      </a>
+      <a href="https://github.com/AbbosbekQodirov">
+        <h4>
+          <FaGithub /> github.com/AbbosbekQodirov
+        </h4>
+      </a>
+      <a href="https://www.linkedin.com/in/abbosbekqodirov/">
+        <h4>
+          <FaLinkedin /> abbosbekqodirov
+        </h4>
+      </a>
+      <a href="https://abbosbek-creative-portfolio.netlify.app/">
+        <h4>
+          <GiEarthAmerica /> https://abbosbek-creative-portfolio.uz
+        </h4>
+      </a>
+    </div>
+  );
 }
 
 export default Contact
